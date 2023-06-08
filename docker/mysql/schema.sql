2d390b5fe4bc841af0d0a78db22d166480511cfb
ziyaoxie <ziyaoxie@outlook.com>
2023-06-08 22:21:53 +0800

    docker: Add mysql dockerfile
    Signed-off-by: ziyaoxie <ziyaoxie@outlook.com>

---
+++ docker/mysql/schema.sql
@@ -0,0 +1,8 @@
+--
+-- Privileges
+--
+
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '5KnU9&Paqw6r%D$p';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
